dc5532a8e1948e3b058001366a4dff6efd572b2e
Krzysztof Śmiałek <admin@avensome.net>
2016-04-02 14:44:23 +0200

    Rotating platform with click raycasting

---
+++ Assets/Scripts/MouseCameraControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCameraControl : MonoBehaviour {
+    public Transform rotationHandle;
+    public Transform translationHandle;
+    public int mouseButton;
+
+    public float nearDistance;
+    public float farDistance;
+
+    public float rotationRate;
+    public float zoomingRate;
+
+    private Vector3? dragOrigin = null;
+
+    void Start() {
+        if (rotationHandle == null) {
+            rotationHandle = transform;
+        }
+
+        if (translationHandle == null) {
+            translationHandle = transform;
+        }
+    }
+
+    void LateUpdate() {
+        if (dragOrigin == null && Input.GetMouseButtonDown(mouseButton)) {
+            dragOrigin = Input.mousePosition;
+        } else if (dragOrigin != null && Input.GetMouseButtonUp(mouseButton)) {
+            dragOrigin = null;
+        }
+
+        if (dragOrigin != null) {
+            Vector3 newDragOrigin = Input.mousePosition;
+            Vector3 delta = newDragOrigin - (Vector3) dragOrigin;
+
+            rotationHandle.Rotate(Vector3.up, delta.x * rotationRate);
+            var zValue = translationHandle.localPosition.z + delta.y * zoomingRate;
+            float clampedZValue = Mathf.Clamp(zValue, farDistance, nearDistance);
+            translationHandle.localPosition = new Vector3(0, 0, clampedZValue);
+
+            dragOrigin = newDragOrigin;
+        }
+    }
+}
---
+++ Assets/Scripts/MoveToClickPoint.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class MoveToClickPoint : ClickReceiver {
+    public override void receive(RaycastHit hit) {
+        transform.position = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+    }
+}
